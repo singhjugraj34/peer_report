31cb11f6b31c5b6290d3fe77dd82ba2ba1fe842f
singhjugraj34 <singhjugraj34@gmail.com>
2025-09-18 12:37:48 -0600

    Update main_cisco.py

--- main_cisco.py
+++ main_cisco.py
@@ -6,7 +6,7 @@ import re
 from getpass import getpass
 from datetime import datetime
 from netmiko import ConnectHandler
-from netmiko.ssh_exception import NetMikoAuthenticationException, NetMikoTimeoutException
+from netmiko.ssh_exceptions import NetMikoAuthenticationException, NetMikoTimeoutException
 
 # ----- Simple thresholds -----
 GREEN_MAX = 0.60   # <60% = green
@@ -269,5 +269,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
